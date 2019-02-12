115de8d4ceb95d5fcac94b0b97cb41329dedec55
Javier Mendoza <javier.mendoza@students.makeschool.com>
2019-02-12 14:00:10 -0800

    Users can sign up

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
+require('dotenv').config();
+var cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 const express = require('express')
 const app = express()
 var exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 
+
 // Use Body Parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(cookieParser()); // Add this after you initialize express.
 
 // Add after body parser initialization!
 app.use(expressValidator());
@@ -20,6 +26,7 @@ require('./data/reddit-db');
 
 require('./controllers/posts.js')(app);
 require('./controllers/comments.js')(app);
+require('./controllers/auth.js')(app);
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
