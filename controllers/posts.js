b43514b8637801c95fba38f69974f068cfb465ad
Javier Mendoza <javier.mendoza@students.makeschool.com>
2019-02-15 22:56:34 -0800

    fix comments

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,18 +1,20 @@
 const Post = require('../models/post');
+const User = require('../models/user');
 
 module.exports = app => {
-    //INDEX
-    app.get("/", (req, res) => {
-    var currentUser = req.user;
-    
-    Post.find({})
+    // INDEX
+    app.get('/', (req, res) => {
+        var currentUser = req.user;
+        // res.render('home', {});
+        console.log(req.cookies);
+        Post.find().populate('author')
         .then(posts => {
-        res.render("posts-index", { posts, currentUser });
+            res.render('posts-index', { posts, currentUser });
+            // res.render('home', {});
+        }).catch(err => {
+            console.log(err.message);
         })
-        .catch(err => {
-        console.log(err.message);
-        });
-    });
+    })
       
     // NEW
     app.get('/posts/new', (req, res) => {
@@ -22,36 +24,52 @@ module.exports = app => {
     // CREATE
     app.post("/posts/new", (req, res) => {
         if (req.user) {
-        var post = new Post(req.body);
-    
-        post.save(function(err, post) {
-            return res.redirect(`/`);
-        });
+            var post = new Post(req.body);
+            post.author = req.user._id;
+
+            post
+                .save()
+                .then(post => {
+                    return User.findById(req.user._id);
+                })
+                .then(user => {
+                    user.posts.unshift(post);
+                    user.save();
+                    // REDIRECT TO THE NEW POST
+                    res.redirect(`/posts/${post._id}`);
+                })
+                .catch(err => {
+                    console.log(err.message);
+                });
         } else {
-        return res.status(401); // UNAUTHORIZED
+            return res.status(401); // UNAUTHORIZED
         }
     });
 
-    //POST
-    app.get("/posts/:id", function(req, res) {
+    //SHOW
+    app.get("/posts/:id", function (req, res) {
+        var currentUser = req.user;
         // LOOK UP THE POST
-        Post.findById(req.params.id).populate('comments')
-        .then((post) => {
-            res.render('posts-show', { post })
-        }).catch((err) => {
-            console.log(err.message)
-        });
+
+        Post.findById(req.params.id).populate('comments').populate('author')
+            .then(post => {
+                res.render("posts-show", { post, currentUser });  
+            })
+            .catch(err => {
+                console.log(err.message);
+            });
     });
 
     // SUBREDDIT
-    app.get("/n/:subreddit", function(req, res) {
-        Post.find({ subreddit: req.params.subreddit })
-        .then(posts => {
-            res.render("posts-index", { posts });
-        })
-        .catch(err => {
-            console.log(err);
-        });
-    });
+    app.get("/n/:subreddit", function (req, res) {
+        var currentUser = req.user;
+        Post.find({ subreddit: req.params.subreddit }).populate('author')
+            .then(posts => {
+                res.render("posts-index", { posts, currentUser });
+            })
+            .catch(err => {
+                console.log(err);
+            });
+});
     
 };
